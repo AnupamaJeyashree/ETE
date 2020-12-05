0aba3d994a181cc0063e80f5a3bfc90f02ee3210
AnupamaJeyashree <68342817+AnupamaJeyashree@users.noreply.github.com>
2020-12-05 09:20:26 +0530

    Update config.php

--- stulogin/dbconfig/config.php
+++ stulogin/dbconfig/config.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","YW5skZa3uv9On5fz") or die("Unable to connect");
+$con = mysqli_connect("127.0.0.1","root","YW5skZa3uv9On5fz") or die("Unable to connect");
 mysqli_select_db($con,'ete');
 ?>
